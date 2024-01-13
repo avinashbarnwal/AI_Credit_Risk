604df4ebbd5e6440017ac4f5a52443c08306be81
Avinash Barnwal <avinashbarnwal123@gmail.com>
2024-01-14 00:52:08 +0530

    updated the data prep

---
+++ explainable_AI/src/data_prep.py
@@ -0,0 +1,89 @@
+# numpy and pandas for data manipulation
+import numpy as np
+import pandas as pd 
+
+# sklearn preprocessing for dealing with categorical variables
+from sklearn.preprocessing import LabelEncoder
+
+# File system manangement
+import os
+
+# Suppress warnings 
+import warnings
+warnings.filterwarnings('ignore')
+
+# matplotlib and seaborn for plotting
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+app_train = pd.read_csv('./data/application_train.csv')
+print('Training data shape: ', app_train.shape)
+app_train.head()
+
+
+app_test = pd.read_csv('../input/application_test.csv')
+print('Testing data shape: ', app_test.shape)
+app_test.head()
+
+# Function to calculate missing values by column# Funct 
+def missing_values_table(df):
+        # Total missing values
+        mis_val = df.isnull().sum()
+        
+        # Percentage of missing values
+        mis_val_percent = 100 * df.isnull().sum() / len(df)
+        
+        # Make a table with the results
+        mis_val_table = pd.concat([mis_val, mis_val_percent], axis=1)
+        
+        # Rename the columns
+        mis_val_table_ren_columns = mis_val_table.rename(
+        columns = {0 : 'Missing Values', 1 : '% of Total Values'})
+        
+        # Sort the table by percentage of missing descending
+        mis_val_table_ren_columns = mis_val_table_ren_columns[
+            mis_val_table_ren_columns.iloc[:,1] != 0].sort_values(
+        '% of Total Values', ascending=False).round(1)
+        
+        # Print some summary information
+        print ("Your selected dataframe has " + str(df.shape[1]) + " columns.\n"      
+            "There are " + str(mis_val_table_ren_columns.shape[0]) +
+              " columns that have missing values.")
+        
+        # Return the dataframe with missing information
+        return mis_val_table_ren_columns
+
+# Missing values statistics
+missing_values = missing_values_table(app_train)
+
+
+# Create a label encoder object
+le = LabelEncoder()
+le_count = 0
+
+# Iterate through the columns
+for col in app_train:
+    if app_train[col].dtype == 'object':
+        # If 2 or fewer unique categories
+        if len(list(app_train[col].unique())) <= 2:
+            # Train on the training data
+            le.fit(app_train[col])
+            # Transform both training and testing data
+            app_train[col] = le.transform(app_train[col])
+            app_test[col] = le.transform(app_test[col])
+            
+            # Keep track of how many columns were label encoded
+            le_count += 1
+
+
+# one-hot encoding of categorical variables
+app_train = pd.get_dummies(app_train)
+app_test = pd.get_dummies(app_test)
+
+print('Training Features shape: ', app_train.shape)
+print('Testing Features shape: ', app_test.shape)
+
+from econml.solutions.causal_analysis import CausalAnalysis
+ca = CausalAnalysis(top_features,categorical,heterogeneity_inds=None,classification=True,nuisance_models="automl",heterogeneity_model="forest",n_jobs=-1,random_state=123,)
+ca.fit(x_train, y_train.values)
